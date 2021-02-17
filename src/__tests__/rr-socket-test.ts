aa0a827dfb9220621657e0a12d931ea23e0fcbe8
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2021-02-16 22:42:02 -0500

    WIP: RRSocket

---
+++ src/__tests__/rr-socket-test.ts
@@ -0,0 +1,239 @@
+import net, { Socket } from "net";
+import RRSocket from "../transport/rr-socket";
+
+describe("RRSocket", () => {
+    it ("should not attempt to connect when setting address/port while disconnected", (done) => {
+        const client: RRSocket = new RRSocket();
+
+        client.on("close", () => {
+            throw new Error("Should not have fired 'close' event");
+        });
+
+        client.address = "localhost";
+        client.port = 8080;
+
+        done();
+    });
+
+    it("should connect and disconnect cleanly", async (done) => {
+        const events: string[] = [];
+
+        let serverPeer: Socket;
+        const server = net.createServer(socket => {
+            serverPeer = socket;
+            events.push("server-connect");
+        });
+
+        server.listen(1735);
+
+        const client: RRSocket = new RRSocket({
+            address: "localhost",
+            port: 1735,
+            ident: "test-1"
+        });
+
+        client.on("connected", () => {
+            events.push("connected");
+
+            // Once we're connected, disconnect
+            client.disconnect();
+        });
+
+        client.on("data", () => {
+            events.push("data");
+        });
+
+        client.on("close", () => {
+            events.push("close");
+
+            expect(events).toEqual(["server-connect", "connected",  "close"])
+            server.close(() => {
+                done();
+            });
+        });
+
+        client.connect();
+
+    });
+
+    it ("should attempt reconnection if the server is unavailable", async (done) => {
+        const events: string[] = [];
+
+        const client: RRSocket = new RRSocket({
+            address: "localhost",
+            port: 1735,
+            reconnectDelay: 1000,
+            ident: "test-2"
+        });
+
+        client.on("connected", () => {
+            events.push("connected");
+        });
+
+        client.on("close", () => {
+            events.push("close");
+        });
+
+        client.on("reconnectAttempt", () => {
+            events.push("reconnectAttempt");
+        });
+
+        client.connect();
+
+        setTimeout(() => {
+            client.disconnect();
+            expect(events).toEqual(["reconnectAttempt", "reconnectAttempt", "reconnectAttempt"]);
+            done();
+        }, 3500);
+    });
+
+    it ("should attempt reconnection if the server closes the connection", async (done) => {
+        const events: string[] = [];
+
+        const client: RRSocket = new RRSocket({
+            address: "localhost",
+            port: 1735,
+            ident: "test-3"
+        });
+
+        let numClients = 0;
+        const server = net.createServer(socket => {
+            events.push("server-connect");
+            numClients++;
+
+            if (numClients === 1) {
+                // Disconnect the first socket
+                socket.destroy();
+            }
+            else if (numClients === 2) {
+                setTimeout(() => {
+                    client.disconnect();
+                }, 500);
+
+            }
+        });
+
+        server.listen(1735);
+
+        client.on("connected", () => {
+            events.push("connected");
+        });
+
+        client.on("close", () => {
+            events.push("close");
+            if (numClients === 2) {
+
+                expect(events).toEqual([
+                    "server-connect",
+                    "connected",
+                    "reconnectAttempt",
+                    "server-connect",
+                    "connected",
+                    "close"
+                ]);
+
+                server.close(() => {
+                    done();
+                });
+            }
+        });
+
+        client.on("reconnectAttempt", () => {
+            events.push("reconnectAttempt");
+        });
+
+        client.connect();
+    });
+
+    it ("should handle reconnects to different servers", async (done) => {
+        const events: string[] = [];
+        const buffers: Buffer[] = [];
+
+        let numConnections = 0;
+
+        const server1 = net.createServer(socket => {
+            events.push("server1-connect");
+            socket.write("abc");
+        });
+
+        const server2 = net.createServer(socket => {
+            events.push("server2-connect");
+            socket.write("def");
+        })
+
+        server1.listen(1735);
+        server2.listen(1736);
+
+        const client: RRSocket = new RRSocket({
+            address: "localhost",
+            port: 1735,
+            ident: "test-4"
+        });
+
+        client.on("data", (data: Buffer) => {
+            buffers.push(data);
+        });
+
+        client.on("connected", () => {
+            numConnections++;
+
+            if (numConnections === 1) {
+                // Switch ports to the other server
+                setTimeout(() => {
+                    client.port = 1736;
+                }, 500);
+            }
+            else if (numConnections === 2) {
+                setTimeout(() => {
+                    client.port = 1735;
+                }, 500);
+            }
+            else if (numConnections === 3) {
+                setTimeout(() => {
+                    const result = Buffer.concat(buffers);
+                    expect(result).toEqual(Buffer.from("abcdefabc"));
+                    client.disconnect();
+                    server1.close(() => {
+                        server2.close(() => {
+                            done();
+                        });
+                    });
+                }, 500);
+
+            }
+        });
+
+        client.connect();
+    });
+
+    it("should connect when a server eventually starts up", async (done) => {
+        const client: RRSocket = new RRSocket({
+            address: "localhost",
+            port: 1735,
+            reconnectDelay: 500,
+            ident: "test-5"
+        });
+
+        const server = net.createServer();
+
+        let reconnectCount = 0;
+
+        client.on("connected", () => {
+            client.disconnect();
+            server.close(() => {
+                expect(reconnectCount).toBeGreaterThan(2);
+                done();
+            });
+        });
+
+        client.on("reconnectAttempt", () => {
+            reconnectCount++;
+        })
+
+        client.connect();
+
+        setTimeout(() => {
+            server.listen(1735);
+        }, 3000);
+    });
+});
---
+++ src/transport/rr-socket.ts
@@ -0,0 +1,155 @@
+import { EventEmitter } from "events";
+import { Socket } from "net";
+
+export interface RRSocketOptions {
+    address?: string;
+    port?: number;
+    reconnectDelay?: number;
+    ident?: string;
+}
+
+/**
+ * Re-targetable, Re-connecting Socket
+ *
+ * This socket wrapper allows re-targetting (having the same socket object
+ * but connecting to a different endpoint), and re-connection (tries to
+ * connect over and over)
+ */
+export default class RRSocket extends EventEmitter {
+    private _socket: Socket;
+    private _socketConnected: boolean = false;
+    private _ident: string = "";
+
+    private _reconnectTimeoutDelayMs: number = 1000;
+    private _reconnectTimeoutHandle: NodeJS.Timeout;
+
+    private _address: string = "";
+    private _port: number = 0;
+
+    constructor(options?: RRSocketOptions) {
+        super();
+
+        if (options) {
+            if (options.address !== undefined) {
+                this._address = options.address;
+            }
+
+            if (options.port !== undefined) {
+                this._port = options.port;
+            }
+
+            if (options.reconnectDelay !== undefined) {
+                this._reconnectTimeoutDelayMs = options.reconnectDelay;
+            }
+
+            if (options.ident !== undefined) {
+                this._ident = options.ident;
+            }
+        }
+    }
+
+    public get address(): string {
+        return this._address;
+    }
+
+    public set address(val: string) {
+        if (val !== this._address) {
+            this._address = val;
+            if (this._socketConnected) {
+                this.disconnect();
+                this.connect();
+            }
+        }
+    }
+
+    public get port(): number {
+        return this._port;
+    }
+
+    public set port(val: number) {
+        if (val !== this._port) {
+            this._port = val;
+
+            if (this._socketConnected) {
+                this.disconnect();
+                this.connect();
+            }
+        }
+    }
+
+    public connect() {
+        if (this._socketConnected) {
+            return;
+        }
+
+        this._socket = new Socket();
+        this._hookupEvents();
+
+        this._doConnect();
+    }
+
+    public disconnect() {
+        clearTimeout(this._reconnectTimeoutHandle);
+
+        if (!this._socketConnected) {
+            console.log(`[${this._ident}] socket not connected during disconnect`);
+            return;
+        }
+
+        if (this._socket) {
+            this._socket.removeAllListeners();
+            this._socket.end();
+            this._socketConnected = false;
+            this.emit("close", false);
+
+            this._socket = undefined;
+            this._socketConnected = false;
+        }
+    }
+
+    private _hookupEvents() {
+        this._socket.on("data", data => {
+            this.emit("data", data);
+        });
+
+        this._socket.on("close", (hadError: boolean) => {
+            if (this._socketConnected) {
+                // We were previously connected, therefore this must
+                // be a disconnection and we should try to reconnect
+                this._attemptReconnect();
+            }
+
+            this._socketConnected = false;
+        });
+
+        this._socket.on("error", err => {
+            if (err.message.indexOf("ECONNREFUSED") !== -1) {
+                this._attemptReconnect();
+            }
+        });
+
+        this._socket.on("end", () => {
+            this._socket.end();
+            this._socketConnected = false;
+
+            this._attemptReconnect();
+        })
+    }
+
+    private _attemptReconnect() {
+        clearTimeout(this._reconnectTimeoutHandle);
+        this._reconnectTimeoutHandle = setTimeout(() => {
+            console.log(`[${this._ident}] Reconnecting to ${this._address}:${this._port} (Socket Closed)`);
+            this.emit("reconnectAttempt");
+            this.connect();
+        }, this._reconnectTimeoutDelayMs);
+    }
+
+    private _doConnect() {
+        this._socket.connect(this._port, this._address, () => {
+            this._socketConnected = true;
+            console.log(`[${this._ident}] Connected to ${this._address}:${this._port}`);
+            this.emit("connected");
+        });
+    }
+}
