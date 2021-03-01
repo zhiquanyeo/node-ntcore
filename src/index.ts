c719a9baca86072fef9f6d6c0d81783cbf801b17
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2021-03-01 17:40:51 -0500

    Fix main package index

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,8 @@
 import NetworkTableInstance from "./networktables/network-table-instance";
 export { NetworkTableInstance };
 
-// NT Internals
-import NTEntry, { NTEntryFlags, NTEntryValue } from "./protocol/nt-entry";
-import NTParticipant, { NTParticipantOptions } from "./protocol/nt-participant";
-import NTServer, { NTServerOptions } from "./protocol/nt-server";
-import NTClient, { NTClientOptions } from "./protocol/nt-client";
-import * as NTTypes from "./protocol/nt-types";
+import NetworkTable from "./networktables/network-table";
+export { NetworkTable };
 
-// V3 NT Internals
-import * as V3Types from "./protocol/v3/v3-types";
-import * as V3Messages from "./protocol/v3/v3-messages";
-import V3NTServer, { V3ServerOptions } from "./protocol/v3/v3-nt-server";
-import V3NTClient, { V3ClientOptions } from "./protocol/v3/v3-nt-client";
+import NetworkTableEntry, { NetworkTableEntryFlags } from "./networktables/network-table-entry";
+export { NetworkTableEntry, NetworkTableEntryFlags };
